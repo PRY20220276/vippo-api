dcfde61167c7ad15b5f81b79a5bdfa3a104ffd33
MarcoMnrq <63822742+MarcoMnrq@users.noreply.github.com>
2023-03-13 14:51:13 -0500

    fix: auth jwt secret

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET,
+      secret: process.env.JWT_SECRET || ' ',
       signOptions: { expiresIn: '30m' },
     }),
   ],
