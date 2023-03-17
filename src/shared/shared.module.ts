6f366bb5a2ed2551b634386b2edf67017642d33f
MarcoMnrq <63822742+MarcoMnrq@users.noreply.github.com>
2023-03-16 20:25:56 -0500

    feat: add secret service

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,11 +1,22 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './services/prisma.service';
+import { SecretService } from './services/secret.service';
 import { VideoAnalysisService } from './services/video-analysis.service';
 import { VideoUploadService } from './services/video-upload.service';
 
 @Global()
 @Module({
-  exports: [PrismaService, VideoUploadService, VideoAnalysisService],
-  providers: [PrismaService, VideoUploadService, VideoAnalysisService],
+  exports: [
+    PrismaService,
+    VideoUploadService,
+    VideoAnalysisService,
+    SecretService,
+  ],
+  providers: [
+    PrismaService,
+    VideoUploadService,
+    VideoAnalysisService,
+    SecretService,
+  ],
 })
 export class SharedModule {}
